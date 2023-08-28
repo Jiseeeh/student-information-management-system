3e748fdf7b58ee9c32b7aeb039e030526331adc3
Jireh-sama <106262567+Jireh-sama@users.noreply.github.com>
2023-08-28 12:23:54 +0800

    added userprofile UI (#5)
    Co-authored-by: John Carlo Camara <johncarlo.camara1@gmail.com>

--- src/sims/StudentInformationManagementSystem.java
+++ src/sims/StudentInformationManagementSystem.java
@@ -4,6 +4,8 @@
  */
 package sims;
 
+
+
 /**
  *
  * @author johnc
@@ -14,7 +16,7 @@ public class StudentInformationManagementSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
---
+++ src/sims/ui/Colors.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sims.ui;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Jireh
+ */
+public class Colors {
+    Color primaryColor = new Color(217, 217, 217);
+    Color hoverColor = new Color(205, 205, 205);
+}
