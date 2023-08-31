f4a57c0bc90648ab3534222602d3c60f87f755b6
Jiseeeh <johncarlo.camara1@gmail.com>
2023-08-31 17:41:35 +0800

    feat: add Fee model

---
+++ src/sims/model/Fee.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sims.model;
+
+/**
+ *
+ * @author johnc
+ */
+public class Fee {
+    private String title;
+    private String dueDate;
+    private boolean isPending;
+    private double amount;
+
+    public Fee(String title, String dueDate, double amount) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.amount = amount;
+        this.isPending = true;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @return the dueDate
+     */
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    /**
+     * @return the isPending
+     */
+    public boolean isIsPending() {
+        return isPending;
+    }
+
+    /**
+     * @return the amount
+     */
+    public double getAmount() {
+        return amount;
+    }
+
+}
