2484b56825f466f97bb4cd9e358c3e0b39c8e922
Jiseeeh <johncarlo.camara1@gmail.com>
2023-09-01 20:58:01 +0800

    feat: enable running on main

--- src/sims/StudentInformationManagementSystem.java
+++ src/sims/StudentInformationManagementSystem.java
@@ -4,7 +4,7 @@
  */
 package sims;
 
-
+import sims.ui.MainFrame;
 
 /**
  *
@@ -16,7 +16,27 @@ public class StudentInformationManagementSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            var mainFrame = new MainFrame();
+            mainFrame.setLocationRelativeTo(null);
+            mainFrame.setVisible(true);
+        });
     }
-    
 }
