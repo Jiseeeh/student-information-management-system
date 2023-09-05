ffde58d0c9ee22cf9034889cf178fcd682f1745a
Jiseeeh <johncarlo.camara1@gmail.com>
2023-09-05 12:28:07 +0800

    style: add dark mode

--- src/sims/StudentInformationManagementSystem.java
+++ src/sims/StudentInformationManagementSystem.java
@@ -4,6 +4,9 @@
  */
 package sims;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import sims.ui.MainFrame;
 
 /**
@@ -16,21 +19,14 @@ public class StudentInformationManagementSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            /* Create and display the form */
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (UnsupportedLookAndFeelException ex) {
+            System.out.println("Error on setting look and feel.");
         }
+        
+   
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
